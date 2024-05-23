c3f53307e0c62618879ac0ecd55d34951b0a223d
Lovin4ik <rasteniaros@gmail.com>
2024-05-23 20:39:02 +0300

    fix modal

--- pages/webstudio/js/burger.js
+++ pages/webstudio/js/burger.js
@@ -1,10 +1,10 @@
-const burgerMenu = document.querySelector('.burger__menu')
-const burgerClose = document.querySelector('.burger__close')
-const burgerOpen = document.querySelector('.burger__open')
+const burgerMenu = document.querySelector('.burger__menu');
+const burgerClose = document.querySelector('.burger__close');
+const burgerOpen = document.querySelector('.burger__open');
 
 const burger = () => {
-	burgerMenu.classList.toggle('burger__off')
-}
+	burgerMenu.classList.toggle('burger__off');
+};
 
-burgerOpen.addEventListener('click', burger)
-burgerClose.addEventListener('click', burger)
+burgerOpen.addEventListener('click', burger);
+burgerClose.addEventListener('click', burger);
