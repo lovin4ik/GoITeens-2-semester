10594d88d0e724e0041e2315090ebc5008a39b46
Lovin4ik <rasteniaros@gmail.com>
2024-05-23 19:34:45 +0300

    no-scroll

--- pages/webstudio/js/modal.js
+++ pages/webstudio/js/modal.js
@@ -2,10 +2,12 @@ const modal = document.querySelector('.modal');
 const modalWrapper = document.querySelector('.modal__wrapper');
 const btnOpen = document.querySelector('.hero__btn');
 const btnClose = document.querySelector('.modal__close');
+const body = document.querySelector('.app');
 
 const toggle = () => {
 	modal.classList.toggle('off');
 	modalWrapper.classList.toggle('modal__on');
+	body.classList.toggle('no-scroll');
 };
 
 btnOpen.addEventListener('click', toggle);
