ef02aaca5f8b7c87556018087cd7c66f38aa5e1d
Lovin4ik <rasteniaros@gmail.com>
2024-05-27 19:23:15 +0300

    modalWindow

--- pages/webstudio/js/modal.js
+++ pages/webstudio/js/modal.js
@@ -1,14 +1,19 @@
-const modal = document.querySelector('.modal');
-const modalWrapper = document.querySelector('.modal__wrapper');
-const btnOpen = document.querySelector('.hero__btn');
-const btnClose = document.querySelector('.modal__close');
-const body = document.querySelector('.app');
-
-const toggle = () => {
-	modal.classList.toggle('off');
-	modalWrapper.classList.toggle('modal__on');
-	body.classList.toggle('no-scroll');
+const refs = {
+	modal: document.querySelector('.modal'),
+	modalWrapper: document.querySelector('.modal__wrapper'),
+	btnOpen: document.querySelector('.hero__btn'),
+	btnClose: document.querySelector('.modal__close'),
+	body: document.querySelector('.app'),
 };
 
-btnOpen.addEventListener('click', toggle);
-btnClose.addEventListener('click', toggle);
+const toggle = (e) => {
+	refs.modal.classList.toggle('off');
+	refs.modalWrapper.classList.toggle('modal__on');
+	refs.body.classList.toggle('no-scroll');
+};
+refs.modalWrapper.addEventListener('click', (e) => {
+	e.stopPropagation();
+});
+refs.btnOpen.addEventListener('click', toggle);
+refs.btnClose.addEventListener('click', toggle);
+refs.modal.addEventListener('click', toggle);
