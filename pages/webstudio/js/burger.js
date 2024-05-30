2d5d9c59895aaf90158f666d27cdda0c213dab2e
Lovin4ik <rasteniaros@gmail.com>
2024-05-30 19:04:33 +0300

    22

--- pages/webstudio/js/burger.js
+++ pages/webstudio/js/burger.js
@@ -1,9 +1,11 @@
 const burgerMenu = document.querySelector('.burger__menu');
 const burgerClose = document.querySelector('.burger__close');
 const burgerOpen = document.querySelector('.burger__open');
+const body = document.querySelector('.app');
 
 const burger = () => {
-	burgerMenu.classList.toggle('burger__off');
+	burgerMenu.classList.toggle('burger__on');
+	body.classList.toggle('no-scroll');
 };
 
 burgerOpen.addEventListener('click', burger);
