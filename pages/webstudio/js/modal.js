fbb45a8f49111d669965db1ef9b5ac99c0cb7af2
Valera <122011576+lovin4ik@users.noreply.github.com>
2024-06-08 09:36:07 +0300

    31 add hot keys in webStudio

--- pages/webstudio/js/modal.js
+++ pages/webstudio/js/modal.js
@@ -6,15 +6,26 @@ const refs = {
 	body: document.querySelector('.app'),
 };
 
+let isActive = false
+
 const toggle = (e) => {
+	isActive =! isActive
 	refs.modal.classList.toggle('off');
 	refs.modalWrapper.classList.toggle('modal__on');
 	refs.body.classList.toggle('no-scroll');
 };
 
+document.addEventListener('keydown', e => {
+	if (e.key === 'Escape') {
+		if (isActive === true) {
+			toggle()
+		} else return
+	} else return
+})
 refs.modalWrapper.addEventListener('click', (e) => {
 	e.stopPropagation();
 });
+
 refs.btnOpen.addEventListener('click', toggle);
 refs.btnClose.addEventListener('click', toggle);
 refs.modal.addEventListener('click', toggle);
