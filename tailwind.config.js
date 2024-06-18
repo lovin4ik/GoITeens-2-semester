e512c0caf4cc38a90c9d77748b70737d100c0162
Valera <122011576+lovin4ik@users.noreply.github.com>
2024-06-18 19:17:03 +0300

    next 38

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['*.html', './pages/**/*.html'],
+	content: ['.html', './pages/**/*.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				explorer: {
+					yellow: '#DCCA87',
+				},
+			},
+		},
+		fontFamily: {
+			'open-sans': 'Open Sans, sans-serif',
+			cormorant: 'Cormorant Upright, serif',
+			poppins: 'Poppins, sans-serif',
+		},
 	},
 	plugins: [],
 };
